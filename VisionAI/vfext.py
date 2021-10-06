9e251445017b1e7e83d993cf6a966094b2255ca9
Coldwove <alexander7564@gmail.com>
2021-10-06 21:48:13 +1100

    Create vfext.py

---
+++ VisionAI/vfext.py
@@ -0,0 +1,35 @@
+# For User arguments
+import sys
+# OpenCV
+import cv2
+
+# CLI Arguments
+# eg: Main.py video.mp4 0 100 1000
+str(sys.argv)
+
+# Open Video
+cap= cv2.VideoCapture(sys.argv[1])
+
+# CLI Argument frame start position
+i = 2
+
+# Loop while Current read position has not reached end of CLI args
+while (i != len(sys.argv)):
+	# Set Read location
+	cap.set (1,int(sys.argv[i]))
+	# Execute Read
+	ret, frame = cap.read()
+	
+	# Error Checking
+	if ret == False:
+		print ('Frame Error')
+		break
+		
+	# Write frame
+	cv2.imwrite (str(sys.argv[i])+'.jpg',frame)
+	
+	i += 1
+
+# CleanUp
+cap.release()
+cv2.destroyAllWindows()
